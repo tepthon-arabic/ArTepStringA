4943ec8257418b6c33a2d88e0240c7b33558dbb6
Arabic Source <115221367+Tepthonee@users.noreply.github.com>
2024-02-01 12:55:18 +0300

    Update start.py

--- ArStringTep/modules/start.py
+++ ArStringTep/modules/start.py
@@ -8,8 +8,8 @@ from ArStringTep.utils import add_served_user, keyboard
 @Anony.on_message(filters.command("start") & filters.private & filters.incoming)
 async def f_start(_, message: Message):
     await message.reply_text(
-        text=f"✦ **مرحبًــا  👋** {message.from_user.first_name},\n\n  {Anony.mention},\n**✦ يمكنـك استخـدامي لـ استخـراج كـود الجلسـة لـ تنصيـب  تيبثـون اضغط بدء استخراج الجلسة وابدأ بالاستخراج ✨ .**",
-        reply_markup=keyboarأ
+        text=f"✦ **مرحبًــا  👋** {message.from_user.first_name},\n\nهذا هو  {Anony.mention},\n✦ يمكنـك استخـدامي لـ استخـراج كـود الجلسـة لـ تنصيـب  تيبثـون اضغط بدء استخراج الجلسة وابدأ بالاستخراج ✨",
+        reply_markup=keyboard
         disable_web_page_preview=True,
     )
     await add_served_user(message.from_user.id)
